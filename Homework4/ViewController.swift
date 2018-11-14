db8c998058bbc7b5d71584971ee241a9688d4680
imostoviy <i_mostoviy@ukr.net>
2018-11-14 23:13:08 +0200

    Initial Commit

---
+++ Homework4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework4
+//
+//  Created by Мостовий Ігор on 14.11.18.
+//  Copyright © 2018 Мостовий Ігор. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
