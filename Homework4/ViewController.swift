cfb4e6e7a2b189e9daee578f055184dbc4554a6b
imostoviy <i_mostoviy@ukr.net>
2018-11-15 00:05:17 +0200

    first part

--- Homework4/ViewController.swift
+++ Homework4/ViewController.swift
@@ -15,6 +15,64 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var labelchangedByButton: UILabel!
+    var n = 1
+    @IBOutlet weak var labelChangedByFirstSecond: UILabel!
+    @IBOutlet weak var firstSecond: UISegmentedControl!
+    @IBOutlet weak var labelChangedByTextField: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var labelChangedBySlider: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var switchbtn: UISwitch!
+    @IBOutlet weak var generalView: UIView!
+    @IBOutlet weak var activityspinner: UIActivityIndicatorView!
+    @IBOutlet weak var labelChangedByStepper: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var viewToHoldTheImage: UIView!
+    
+    
+    @IBAction func buttonToChangeTheLabel(_ sender: UIButton) {
+        labelchangedByButton.text = "\(n)th change"
+        n += 1
+    }
+    @IBAction func changingFirstSecond(_ sender: UISegmentedControl) {
+        if(firstSecond.selectedSegmentIndex == 0) {
+            labelChangedByFirstSecond.text = "First"
+        } else {
+            labelChangedByFirstSecond.text = "Second"
+        }
+    }
+    
+    @IBAction func changingTextField(_ sender: UITextField) {
+        labelChangedByTextField.text = textField.text
+    }
+    
+    @IBAction func changedSlider(_ sender: UISlider) {
+        labelChangedBySlider.text = String(slider.value)
+    }
+    
+    @IBAction func changingSwitch(_ sender: UISwitch) {
+        if(switchbtn.isOn) {
+            generalView.backgroundColor = .white
+        } else {
+            generalView.backgroundColor = .black
+        }
+    }
+    
+    @IBAction func buttonForSpinner(_ sender: UIButton) {
+        if(activityspinner.isAnimating) {
+            activityspinner.stopAnimating()
+        } else {
+            activityspinner.startAnimating()
+        }
+    }
+    
+    @IBAction func changeStepper(_ sender: UIStepper) {
+        labelChangedByStepper.text = String(stepper.value)
+    }
+    
+    @IBAction func insertImage(_ sender: UIButton) {
+        viewToHoldTheImage.backgroundColor = UIColor(patternImage: UIImage(named: "1st_task")!)
+    }
 }
 
